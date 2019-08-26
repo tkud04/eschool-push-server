0320e3e912115313f14a90dc3057ed003735b0da
ANonymous <postmaster.richmama@gmail.com>
2019-08-26 13:22:18 +0100

    request and token api

--- index.js
+++ index.js
@@ -62,7 +62,15 @@ express()
 		 au = req.query.au;
 		 tk = req.query.tk;
 		console.log("au: " + au + "\ntk: " + tk);
-		res.send("ok");
+		let uu = mysqlURL + "push-token?student_id=" + au + "&token=" + tk;
+		//save to mysqlURL
+		request(, function (error, response, body) {
+        console.error('error:', error); // Print the error if one occurred
+		
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+		res.send(body);
+     });
 		
 	}
   })
